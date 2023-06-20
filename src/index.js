54e206ab70437b980a6d518c089558d744389052
Michael <dev.michael.gedion@gmail.com>
2023-06-20 05:43:16 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './Card';
+import "tachyons";
+import { robots } from './robots';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+  </div>  
 );
 
 // If you want to start measuring performance in your app, pass a function
