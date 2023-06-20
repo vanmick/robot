4bc1ee965f2e2c8e87854744eaff927d7de24d9a
Michael <dev.michael.gedion@gmail.com>
2023-06-20 06:26:36 +0300

    second commit

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = (props) => {
+const Card = ({name, email, id}) => {
     return (
-        <div className="tc bg-dark-blue moon-gray dib br3 pa3 ma2 grow bw2 shadow" id={props.id}>
-            <img src={`https://robohash.org/${props.id}?200x200`} alt="" />
+        <div className="tc bg-dark-blue moon-gray dib br3 pa3 ma2 grow bw2 shadow" id={id}>
+            <img src={`https://robohash.org/${id}?200x200`} alt="" />
             <div>
-                <h1>{props.name}</h1>
-                <p>{props.email}</p>
+                <h1>{name}</h1>
+                <p>{email}</p>
             </div>       
         </div>
     );
